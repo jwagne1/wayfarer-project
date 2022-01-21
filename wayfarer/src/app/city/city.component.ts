a504c3575f06e99cf4b0a712b637f7515692fcd7
Julio Wagner <jwagnerhn@outlook.com>
2022-01-21 17:45:05 -0500

    Added initial posts to city component. JNA & JW

--- wayfarer/src/app/city/city.component.ts
+++ wayfarer/src/app/city/city.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { cityData } from '../cities/cities_data';
+import { postData } from '../posts';
 
 @Component({
   selector: 'app-city',
@@ -8,26 +9,39 @@ import { cityData } from '../cities/cities_data';
   styleUrls: ['./city.component.css']
 })
 export class CityComponent implements OnInit {
-  cityIndex: string|null = '';
-  name: string|null = '';
+  cityIndex: string | null = '';
+  name: string | null = '';
   city: any;
+  posts: any = postData;
+  filteredPosts: any = [];
+  post: any;
+
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( params => {
+    this.route.paramMap.subscribe(params => {
       this.cityIndex = params.get('id');
 
-      this.city = cityData.find( city => {
+      this.city = cityData.find(city => {
         const paramID: string = params.get('id') || '';
         return city.id === parseInt(paramID);
 
       });
-      
     });
 
-    this.route.queryParams.subscribe( params => {
+    this.route.queryParams.subscribe(params => {
       this.name = params['name'];
+    });
+
+    this.route.paramMap.subscribe(params => {
+      this.cityIndex = params.get('id');
+
+      this.filteredPosts = this.posts.find((post: { city_id: number; }) => {
+        const paramID: string = params.get('id') || '';
+        return post.city_id === parseInt(paramID);
+
+      });
     });
 
   }
