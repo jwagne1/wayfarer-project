a6cdc7e0ff73578c11d41ca247593cadf31b244d
Julio Wagner <jwagnerhn@outlook.com>
2022-01-21 17:08:41 -0500

    Added city pictures to images folder. JNA & JW

--- wayfarer/src/app/cities/cities_data.ts
+++ wayfarer/src/app/cities/cities_data.ts
@@ -11,27 +11,31 @@ export const cityData = [
         id: 2,
         name: 'London',
         country: 'UK',
-        population: '9,002,488'
+        population: '9,002,488',
+        img: '../../assets/images/london.jpeg'
     },
     {
         id: 3,
         name: 'Gibraltar',
         country: 'UK',
-        population: '33,675'
+        population: '33,675',
+        img: '../../assets/images/gibraltar.jpg'
         
     },
     {
         id: 4,
         name: 'Sydney',
         country: 'Australia',
-        population: '5,057,000'
+        population: '5,057,000',
+        img: '../../assets/images/sydney.jpeg'
         
     },
     {
         id: 5,
         name: 'Seattle',
         country: 'USA',
-        population: '3,489,000'
+        population: '3,489,000',
+        img: '../../assets/images/seattle.jpeg'
         
     }
 
